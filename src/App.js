bbb398ba34cc81319c5b576bd0cf5c0902efaa90
Gökhan <gn.ozdemir@gmail.com>
2023-08-02 10:25:14 +0300

    implement search

--- src/App.js
+++ src/App.js
@@ -16,12 +16,12 @@ import sahteVeri from "./sahte-veri";
 import "./App.css";
 
 const App = () => {
-  // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
-  const [gonderiler, setGonderi] = useState(sahteVeri);
+  // Gönderi nesneleri dizisini tutmak için "data" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
+  const [data, setData] = useState(sahteVeri);
 
   // Artık sahteVeri'ye ihtiyacınız olmayacak.
   // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
-  const [aramaKriteri, setAramaKriteri] = useState("Search");
+  const [aramaKriteri, setAramaKriteri] = useState();
 
   const gonderiyiBegen = (gonderiID) => {
     /*
@@ -30,17 +30,40 @@ const App = () => {
       Uygulamanın durumu, React ağacının en üstünde bulunur, ancak iç içe geçmiş bileşenlerin stateleri değiştirememesi adil olmaz!
       Bu fonksiyon, belirli bir gönderinin beğeni sayısını artırılmasına olanak sağlamak amacıyla iç içe geçmiş bileşenlere aktarılır.
 
-	  "setGonderi" yi çağırın ve state ine "posts.map" çağrısını iletin.
+    "setGonderi" yi çağırın ve state ine "posts.map" çağrısını iletin.
       `map` içine iletilen callback aşağıdaki mantığı gerçekleştirir:
         - gönderinin idsi "gonderiID" ile eşleşirse, istenen değerlerle yeni bir gönderi nesnesi döndürün.
         - aksi takdirde, sadece gönderi nesnesini değiştirmeden döndürün.
      */
   };
 
+  const handleSearch = (term) => {
+    // aşağıdaki satır statei güncelliyor
+    setAramaKriteri(term);
+    console.log("handleSearch event", term);
+
+    // data stateini filtrelemek için
+    if (!term) {
+      console.log("term yoksa");
+      setData(sahteVeri);
+    } else {
+      const filteredGonderiler = data.filter((gonderi) => {
+        return gonderi.username.includes(term);
+      });
+
+      setData(filteredGonderiler);
+    }
+  };
+
   return (
     <div className="App">
-      <AramaCubugu />
-      <Gonderiler />
+      <AramaCubugu aramaKriteri={aramaKriteri} aramaFonksiyonu={handleSearch} />
+
+      <div>
+        {data.map((gonderi) => {
+          return <div>{gonderi.username}</div>;
+        })}
+      </div>
 
       {/* Her bileşenin hangi proplara ihtiyaç duyduğunu kontrol edin, eğer ihtiyaç varsa ekleyin! */}
     </div>
